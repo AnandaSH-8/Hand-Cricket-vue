d947f423d9129fb86db20241db1236f6d957d494
Anands-88 <anandsholla8@gmail.com>
2024-01-11 23:58:26 +0530

    player one ui addition

--- hand-cricket/src/components/My Compo/handCricket.vue
+++ hand-cricket/src/components/My Compo/handCricket.vue
@@ -1,6 +1,31 @@
 <template>
     <v-layout class="HCbox">
-        <v-card class="playerOne"></v-card>
+        <v-container class="playerOne">
+            <v-card class="RunImage"></v-card>
+            <v-row id="RunOptions">
+                <v-col>
+                    <img src="../../assets/component Images/Zero.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/One.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/Two.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/Three.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/Four.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/Five.png"/>
+                </v-col>
+                <v-col>
+                    <img src="../../assets/component Images/Six.png"/>
+                </v-col>
+            </v-row>
+        </v-container>
         <v-card class="stadiumDesign"></v-card>
          <v-card class="playerTwo"></v-card>
     </v-layout>
@@ -21,9 +46,38 @@
     border:2px solid red;
 }
 
-.playerTwo, .playerOne{
-    width:25%;
+.RunImage{
+    width:100%;
+    height:50%;
 }
+
+.playerTwo,.playerOne{
+    width:25%;
+    height:40vh;
+    margin-top: 8vh;
+     padding: 0;
+}
+
+.playerOne{
+    height:unset;
+}
+
+.v-row{
+    margin: 0;
+    margin-top: 8%;
+}
+
+
+#RunOptions  .v-col{
+    width:35%;
+    padding-top:3%;
+    padding-bottom:3%;
+}
+
+#RunOptions  .v-col:last-child{
+    padding-left: 38%;
+}
+
 .stadiumDesign{
     width:40%;
     margin:0 7%;
