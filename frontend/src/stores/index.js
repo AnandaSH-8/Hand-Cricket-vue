66b305714e09b7e32287f804e699ec15bcfae3ac
Anands-88 <anandsholla8@gmail.com>
2024-02-20 21:10:04 +0530

    minor change

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -13,13 +13,3 @@ export const useGlobalStore = defineStore('store',() => {
 
   return {runLoader,storeData};
 })
-
-// export const useCounterStore = defineStore('counter', () => {
-//   const count = ref(0)
-//   const doubleCount = computed(() => count.value * 2)
-//   function increment() {
-//     count.value++
-//   }
-
-//   return { count, doubleCount, increment }
-// })
