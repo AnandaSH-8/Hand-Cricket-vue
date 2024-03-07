460e3aa93b40e2acab1c1444f4582d6aaa521b34
Anands-88 <anandsholla8@gmail.com>
2024-03-07 23:46:28 +0530

    setting updates

--- frontend/src/components/My Compo/gameSettings.vue
+++ frontend/src/components/My Compo/gameSettings.vue
@@ -8,18 +8,28 @@
                     Settings                </v-card-title>
             </v-card-item>
             <v-card-text>
-                <v-container class="pt-0 battingContainer" style="border:2px solid red;"> 
+                <v-container class="pt-0 battingContainer"> 
                     <b>Batting</b>
-                    <v-row>
-                        <v-col>Wickets</v-col>
+                    <v-row class="pt-2">
+                        <v-col cols="1"></v-col>
+                        <v-col cols="4" class="bat-options">
+                        <kbd>Wickets</kbd>
+                        <v-select :items="[1,2,3,4,5,10]" density="compact" variant="solo"/>
+                        </v-col>
                     </v-row>
-                </v-container>
-                <v-container class="pt-0 bowlingContainer"  style="border:2px solid red;"> 
+                </v-container>  
+                <hr>
+                <v-container class="pt-0 bowlingContainer" > 
                     <b>Bowling</b>
-                    <v-row>
-                        <v-col>Balls</v-col>
+                    <v-row class="pt-2">
+                        <v-col cols="1"></v-col>
+                        <v-col cols="4" class="bowl-options">
+                        <kbd>Balls</kbd>
+                            <v-text-field density="compact" variant="solo"></v-text-field>
+                        </v-col>
                     </v-row>
                 </v-container>
+                <hr>
             </v-card-text>
         </v-card>
     </v-layout>
@@ -42,6 +52,7 @@
     color:white;
     margin-bottom: 9vh;
     background-color: #f9b851;
+    border-radius: 20px;
 }
 .settingTitle{
     text-align: center;
@@ -77,7 +88,24 @@
     }
 }
 
-.battingContainer > b{
+.battingContainer > b, .bowlingContainer > b{
     font-size: 18px;
 }
+
+.bat-options, .bowl-options{
+    display: flex;
+    justify-content: space-between;
+}
+
+:deep(.bat-options .v-input--density-compact),
+:deep(.bowl-options .v-input--density-compact){
+    max-width:100px !important;
+    height: 25px;
+}
+
+:deep(.bat-options .v-field__field),
+:deep(.bowl-options .v-field__field){
+    height: 25px;
+}
+
 </style>
