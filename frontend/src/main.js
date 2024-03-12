1d682aa7a00ca01ab661f6498293a0cd220e85e2
Anands-88 <anandsholla8@gmail.com>
2024-03-12 21:59:22 +0530

    deploy fixes 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,28 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import GlobalLoader from "./global/loaderStyle.vue"
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import GlobalLoader from "./global/loaderStyle.vue";
 
-import App from './App.vue'
-import router from './router'
-import "./main.css"
+import App from './App.vue';
+import router from './router';
+import "./main.css";
 
 const vuetify = createVuetify({
   components,
   directives
-})
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(vuetify)
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
 app.component('GlobalLoader',GlobalLoader);
 
-app.mount('#app')
+app.mount('#app');
