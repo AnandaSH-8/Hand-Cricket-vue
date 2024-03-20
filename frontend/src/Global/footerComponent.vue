bd15943f0749d390b53e2b3aa8b92c8e9a3e1275
Anands-88 <anandsholla8@gmail.com>
2024-03-20 22:36:23 +0530

    minor changes

--- frontend/src/Global/footerComponent.vue
+++ frontend/src/Global/footerComponent.vue
@@ -1,7 +1,10 @@
 <template>
     <v-layout class="footerLayout">
+        <v-sheet class="footerLogo">
+            <img src="../assets/component Images/hc7-transformed.png" alt="">
+        </v-sheet>
         <v-sheet class="footersheetContent">
-            <h1>Hand Cricket</h1>
+            <h1 style="font-family: 'Gugi';">Hand Cricket</h1>
             <kbd>Made by ASH</kbd>
             <h4>Connect</h4>
             <div>
@@ -9,9 +12,9 @@
                 <v-icon icon="mdi-twitter"></v-icon>
                 <v-icon icon="mdi-linkedin"></v-icon>
             </div>
-            <kbd>© 2024. All rights reserved</kbd>
+            <kbd>©HandCricket 2024</kbd>
         </v-sheet>
-       
+        <v-sheet></v-sheet>
     </v-layout>
 </template>
 <script setup>
@@ -20,11 +23,23 @@ import { ref } from 'vue';
 <style>
 .footerLayout{
     background-color: #f9b851;
-    place-content: center;
+    justify-content: space-between;
+}
+
+.footerLogo{
+    width:100px;
+    height:100px;
+    border:2px solid red;
+}
+
+.footerLogo > img{
+    width:100%;
+    height:100%;
 }
 
 .footerLayout > .footersheetContent{
     background-color: #f9b851;
     text-align: center;
+    color:white;
 }
 </style>
