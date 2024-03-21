71f8db0dccdb0e065ee3afa9d811dc7c98855501
Anands-88 <anandsholla8@gmail.com>
2024-03-21 22:27:02 +0530

    minor changes 2

--- frontend/src/Global/footerComponent.vue
+++ frontend/src/Global/footerComponent.vue
@@ -1,20 +1,20 @@
 <template>
     <v-layout class="footerLayout">
-        <v-sheet class="footerLogo">
+        <!-- <v-sheet class="footerLogo">
             <img src="../assets/component Images/hc7-transformed.png" alt="">
-        </v-sheet>
+        </v-sheet> -->
         <v-sheet class="footersheetContent">
             <h1 style="font-family: 'Gugi';">Hand Cricket</h1>
             <kbd>Made by ASH</kbd>
             <h4>Connect</h4>
-            <div>
-                <v-icon icon="mdi-github"></v-icon>
-                <v-icon icon="mdi-twitter"></v-icon>
-                <v-icon icon="mdi-linkedin"></v-icon>
+            <div class="footerIcons">
+                <a href=""><v-icon icon="mdi-github"></v-icon></a>
+               <a href=""> <v-icon icon="mdi-twitter"></v-icon></a>
+                <a href=""><v-icon icon="mdi-linkedin"></v-icon></a>
             </div>
-            <kbd>©HandCricket 2024</kbd>
+            <kbd>©2024</kbd>
         </v-sheet>
-        <v-sheet></v-sheet>
+        <!-- <v-sheet></v-sheet> -->
     </v-layout>
 </template>
 <script setup>
@@ -23,13 +23,14 @@ import { ref } from 'vue';
 <style>
 .footerLayout{
     background-color: #f9b851;
-    justify-content: space-between;
+    place-content: center;
+    /* justify-content: space-between; */
 }
 
 .footerLogo{
     width:100px;
     height:100px;
-    border:2px solid red;
+    /* border:2px solid red; */
 }
 
 .footerLogo > img{
@@ -41,5 +42,11 @@ import { ref } from 'vue';
     background-color: #f9b851;
     text-align: center;
     color:white;
+}
+
+.footerIcons{
+    border:2px solid red;
+    display: flex;
+    justify-content: space-evenly;
 }
 </style>
