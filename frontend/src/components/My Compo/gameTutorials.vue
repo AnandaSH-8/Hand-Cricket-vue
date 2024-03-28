c0f7e892946c96e25498314d983f0480fb29ea78
Anands-88 <anandsholla8@gmail.com>
2024-03-28 21:40:02 +0530

    minor change

--- frontend/src/components/My Compo/gameTutorials.vue
+++ frontend/src/components/My Compo/gameTutorials.vue
@@ -1,18 +1,15 @@
 <template>
-    <v-layout>
-        <v-card>
-            <v-card-item>
-                <v-card-title>Game Tutorial</v-card-title>
-            </v-card-item>
-            <hr>
-            <v-card-text>
-                
-            </v-card-text>
-        </v-card>
-    </v-layout>
+	<v-layout>
+		<v-card>
+			<v-card-item>
+				<v-card-title>Game Tutorial</v-card-title>
+			</v-card-item>
+			<hr>
+			<v-card-text>
+			</v-card-text>
+		</v-card>
+	</v-layout>
 </template>
 <script>
 </script>
-<style scope>
-    
-</style>
+<style scope></style>
